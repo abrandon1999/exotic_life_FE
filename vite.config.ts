385f93e340600c4a762a01c19c9ee4f4736af318
Brandon Rouse <abrandon1999@yahoo.com>
2026-06-14 23:55:02 -0400

    configure app for https

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import path from "path";
+import fs from "fs";
 // https://vite.dev/config/
 export default defineConfig({
   resolve: {
@@ -13,4 +14,10 @@ export default defineConfig({
     tanstackRouter({ target: "react", autoCodeSplitting: true }),
     react(),
   ],
+  server: {
+    https: {
+      key: fs.readFileSync("./certs/localhost-key.pem"),
+      cert: fs.readFileSync("./certs/localhost.pem"),
+    },
+  },
 });
