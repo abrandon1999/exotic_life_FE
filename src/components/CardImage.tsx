5b2ff7009026a6947f32e1dd43f04c8fffd6857a
Brandon Rouse <abrandon1999@yahoo.com>
2026-06-27 22:44:54 -0400

    implement CardImage Component

--- src/components/CardImage.tsx
+++ src/components/CardImage.tsx
@@ -1,24 +1,117 @@
-import type { CSSProperties } from "react";
 import type { PostImage } from "@/lib/postLoader";
+import type { CSSProperties } from "react";
+import { BACKEND_BASE_URL } from "@/utils/variables";
+import { FaArrowLeft } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
+import { FaRegCircle } from "react-icons/fa";
+import { FaDotCircle } from "react-icons/fa";
+import { useState } from "react";
 interface Props {
   images: PostImage[];
 }
 export default function CardImage({ images }: Props) {
   console.log(images);
+  const [imageIndex, setImageIndex] = useState(0);
+  if (images.length === 0) return null;
+
   return (
-    <div style={imageStyle}>
-      <p style={{ color: "#000", fontSize: "3rem" }}>Image</p>
-    </div>
+    <>
+      <div style={imageSliderContainer}>
+        <div
+          style={{
+            ...imageSliderListContainer,
+            translate: `${-100 * imageIndex}%`,
+            transition: "translate 400ms ease-in-out",
+          }}
+        >
+          {images.map((image) => (
+            <img
+              src={`${BACKEND_BASE_URL}${image.path}`}
+              alt={"user images"}
+              style={imageStyle}
+            />
+          ))}
+        </div>
+        <button
+          style={{ ...imageSliderButton, left: 0 }}
+          onClick={showPrevImage}
+        >
+          <FaArrowLeft size={ICON_SIZE} />
+        </button>
+        <button
+          style={{ ...imageSliderButton, right: 0 }}
+          onClick={showNextImage}
+        >
+          <FaArrowRight size={ICON_SIZE} />
+        </button>
+      </div>
+      <div style={imageSliderDotContainer}>
+        {images.map((_, index) => (
+          <button
+            key={index}
+            style={imageSliderDotButton}
+            onClick={() => setImageIndex(index)}
+          >
+            {imageIndex === index ? <FaDotCircle /> : <FaRegCircle />}
+          </button>
+        ))}
+      </div>
+    </>
   );
+  function showPrevImage() {
+    setImageIndex((index) => {
+      if (index === 0) return images.length - 1;
+      return index - 1;
+    });
+  }
+  function showNextImage() {
+    setImageIndex((index) => {
+      if (index === images.length - 1) return 0;
+      return index + 1;
+    });
+  }
 }
+const ICON_SIZE = 40;
+const imageSliderContainer: CSSProperties = {
+  width: "100%",
+  height: "300px",
+  backgroundColor: "dodgerblue",
+  overflow: "hidden",
+  position: "relative",
+};
+const imageSliderListContainer: CSSProperties = {
+  display: "flex",
+};
 
 const imageStyle: CSSProperties = {
+  flex: "0 0 100%",
+  minWidth: "100%",
+  width: "100%",
+  height: "100%",
+  display: "block",
+  objectFit: "cover",
+};
+const imageSliderButton: CSSProperties = {
+  all: "unset",
+  position: "absolute",
+  top: 0,
+  bottom: 0,
+  padding: "1rem",
+  cursor: "pointer",
+  transition: "background-color 400ms ease-in-out",
+};
+const imageSliderDotContainer: CSSProperties = {
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
-  backgroundColor: "#fff",
-  width: "100%",
-  height: "300px",
-  marginBottom: "20px",
-  borderRadius: "5px",
+};
+const imageSliderDotButton: CSSProperties = {
+  all: "unset",
+  cursor: "pointer",
+  width: "1rem",
+  height: "1rem",
+  transition: "scale 200ms ease-in-out",
+  border: "1px solid black",
+  borderRadius: "50%",
+  margin: "5px",
 };
